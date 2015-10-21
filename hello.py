b5686b7d361df10090a3e96710f523bbc9a0ea99
gavin-lai <gavin.lai@ctl.io>
2015-10-21 10:13:02 -0400

    Added address

--- hello.py
+++ hello.py
@@ -7,10 +7,10 @@ app = Flask(__name__)
 # On CF, get the port number from the environment variable VCAP_APP_PORT
 # When running this app on the local machine, default the port to 8080
 port = int(os.getenv('VCAP_APP_PORT', 8080))
-
+address = (os.getenv('CF_INSTANCE_IP'))
 @app.route('/')
 def hello_world():
-    return 'Hello World! I am running on port ' + str(port)
+    return 'Hello World! I am running on port ' + str(port) + str(address)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=port)
