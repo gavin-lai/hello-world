494442d576be54352af15626320fb09c14d52737
gavin-lai <gavin.lai@ctl.io>
2015-10-22 09:12:26 -0400

    Demo app

--- README.md
+++ README.md
@@ -2,7 +2,7 @@ Hello World in Python on Cloud Foundry using Flask
 ================================================================================
 
 This is a sample application showing how to deploy a simple hello world app
-using Cloud Foundry and the Python build pack.
+using Cloud Foundry and the Python build pack and demostrate the load balancing feature of Appfog.
 
 
 
--- hello.py
+++ hello.py
@@ -4,14 +4,14 @@ import os
 
 app = Flask(__name__)
 
-# On CF, get the port number from the environment variable VCAP_APP_PORT
+# On CF, get the port number from the environment variable VCAP_APP_PORT and server address with CF_INSTANCE_IP
 # When running this app on the local machine, default the port to 8080
 port = int(os.getenv('VCAP_APP_PORT', 8080))
 address = (os.getenv('CF_INSTANCE_IP'))
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! I am running on address:port ' + str(address) + str (':') + str(port)
+    return 'Hello from Appfog World! I am running on address:port ' + str(address) + str (':') + str(port)
 
      
 if __name__ == '__main__':
